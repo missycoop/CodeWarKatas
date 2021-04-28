42294d33dbb204ca143d304ffeec4e27a7204b2e
Missy Coop <m.a.cooper82@gmail.com>
2021-04-28 14:32:15 -0400

    corrected notes

--- strings.js
+++ strings.js
@@ -111,8 +111,6 @@ function order(words){
 
 
 //   Simple, remove the spaces from the string, then return the resultant string.
-
-
   function noSpace(x){
     let arr =  x.split(' ');
     return arr.reduce((a,b)=>a+b);
